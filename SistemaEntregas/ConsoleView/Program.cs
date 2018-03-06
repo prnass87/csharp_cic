6033f4704caf344fc9d530d67008f81173a9ca08
Paulo Roberto Nass <paulonass@hotmail.com>
2018-03-05 21:34:52 -0300

    Criando Menu (com enum)

--- SistemaEntregas/ConsoleView/Program.cs
+++ SistemaEntregas/ConsoleView/Program.cs
@@ -1,12 +1,99 @@
 ﻿using Modelos;
+using System;
 
 namespace ConsoleView
 {
     class Program
     {
-        static void Main(string[] args)
+        //Utiliza quando as opções são FIXAS!
+        enum OpcoesMenuPrincipal
+        {
+            CadastrarCliente = 1,
+            PesquisarCliente = 2,
+            EditarCliente = 3,
+            ExcluirCliente = 4,
+            LimparTela = 5,
+            Sair = 6
+        }
+
+        private static OpcoesMenuPrincipal Menu()
         {
             
+            Console.WriteLine("Escolha sua opção: ");
+            Console.WriteLine("");
+
+            Console.WriteLine("- Clientes -");
+            Console.WriteLine("1 - Cadastrar Novo");
+            Console.WriteLine("2 - Pesquisar Cliente");
+            Console.WriteLine("3 - Editar Cliente");
+
+            Console.WriteLine("- Geral - ");
+            Console.WriteLine("4 - Limpar Tela");
+            Console.WriteLine("5 - Sair");
+
+            string opcao = Console.ReadLine();
+            return (OpcoesMenuPrincipal)int.Parse(opcao);
+            //return int.Parse(Console.ReadLine());
+            //Convert.ToInt32(Console.ReadLine());
+        }
+
+        static void Main(string[] args)
+        {
+            OpcoesMenuPrincipal opcaoDigitada = OpcoesMenuPrincipal.Sair;
+
+            do
+            {
+                opcaoDigitada = Menu();
+
+                switch (opcaoDigitada)
+                {
+                    case OpcoesMenuPrincipal.CadastrarCliente:
+                        break;
+                    case OpcoesMenuPrincipal.PesquisarCliente:
+                        break;
+                    case OpcoesMenuPrincipal.EditarCliente:
+                        break;
+                    case OpcoesMenuPrincipal.ExcluirCliente:
+                        break;
+                    case OpcoesMenuPrincipal.LimparTela:
+                        break;
+                    case OpcoesMenuPrincipal.Sair:
+                        break;
+                    default:
+                        Console.WriteLine("Opção invalida. Tente novamente");
+                        break;
+                }
+            } while (opcaoDigitada != OpcoesMenuPrincipal.Sair);
+        }
+
+        //Métodos: Cliente
+        private static Cliente CadastrarCliente()
+        {
+            Cliente cli = new Cliente();
+
+            Console.Write("Digite o nome: ");
+            cli.Nome = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("Digite o CPF: ");
+            cli.Cpf = Console.ReadLine();
+
+            //Endereco
+            Console.Write("Digite o endereco. Rua: ");
+            cli._Endereco.Rua = Console.ReadLine();
+            Console.Write("Número: ");
+            cli._Endereco.Numero = int.Parse(Console.ReadLine());
+            Console.Write("Complemento: ");
+            cli._Endereco.Complemento = Console.ReadLine();
+            Console.WriteLine();
+
+            return cli;
+        }
+
+        private static Cliente PesquisarCliente()
+        {
+            //TODO: Fazer depois
+            return new Cliente();
         }
     }
 }
