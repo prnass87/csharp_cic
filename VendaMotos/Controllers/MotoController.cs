7676438944a7baef95909412e469c72e821c203e
Paulo Roberto Nass <paulonass@hotmail.com>
2018-04-02 21:34:24 -0300

    Criando a classe MotoController. Criando o projeto WPF para a criação das telas.

--- VendaMotos/Controllers/MotoController.cs
+++ VendaMotos/Controllers/MotoController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,70 @@ using System.Threading.Tasks;
 
 namespace Controllers
 {
-    class MotoController
+    public class MotoController
     {
+        static List<Moto> MinhasMotos = new List<Moto>();
+        static int ultimoID = 0;
+
+        public void SalvarMoto(Moto moto)
+        {
+            int id = ultimoID + 1;
+            ultimoID = 1;
+            moto.MotoID = id;
+            MinhasMotos.Add(moto);
+        }
+
+        public Moto PesquisarPorModelo(string nome)
+        {
+            var m = from x in MinhasMotos
+                    where x.Modelo.ToLower().Contains(nome.Trim().ToLower())
+                    select x;
+
+            if (m != null)
+                return m.FirstOrDefault();
+            else
+                return null;
+        }
+
+        public Moto PesquisarPorID(int idMoto)
+        {
+            var m = from x in MinhasMotos
+                    where x.MotoID.Equals(idMoto)
+                    select x;
+
+            if (m != null)
+                return m.FirstOrDefault();
+            else
+                return null;
+        }
+
+        public void ExcluirMoto(int idMoto)
+        {
+            Moto mto = PesquisarPorID(idMoto);
+            if (mto != null)
+            {
+                MinhasMotos.Remove(mto);
+            }
+        }
+
+        public List<Moto> ListarMotos()
+        {
+            return MinhasMotos;
+        }
+
+        public void EditarMoto(int idMotoEditar, Moto MotoEditada)
+        {
+            Moto MotoEditar = PesquisarPorID(idMotoEditar);
+
+            MotoEditar.Marca = MotoEditada.Marca;
+            MotoEditar.Modelo = MotoEditada.Modelo;
+            MotoEditar.Cilindrada = MotoEditada.Cilindrada;
+            MotoEditar.Ano = MotoEditada.Ano;
+            MotoEditar.Cor = MotoEditada.Cor;
+            MotoEditar.Placa = MotoEditada.Placa;
+            MotoEditar.Valor = MotoEditada.Valor;
+            MotoEditar.Status = MotoEditada.Status;
+
+        }
     }
 }
