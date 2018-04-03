942326df125fbae0c2c4692c39fb21050eec983b
jaqueline oliveira <jaqueline_s.oliveira27@hotmail.com>
2018-04-02 21:35:56 -0300

    Criando Metodo SalvarVendedor  e PesquisarPorNome

--- VendaMotos/Controllers/VendedorController.cs
+++ VendaMotos/Controllers/VendedorController.cs
@@ -12,9 +12,27 @@ namespace Controllers
         static List<Vendedor> MeusVendedores = new List<Vendedor>();
         static int ultimoID = 0;
 
-        public void SalvarVendedor (Vendedor vendedor)
+        public void SalvarVendedor(Vendedor vendedor)
         {
+            int id = ultimoID + 1;
+            ultimoID = id;
+            vendedor.PessoaID = id;
+            MeusVendedores.Add(vendedor);
+
 
         }
+
+          public Vendedor PesquisarPorNome(string nome)
+          {
+                var v = from x in MeusVendedores
+                        where x.Nome.ToLower().Contains(nome.Trim().ToLower())
+                        select x;
+
+                if (v != null)
+                    return v.FirstOrDefault();
+                else
+                    return null;
+            } 
+         
     }
 }
