62f51519701d333b8b545af0cddf086c0c7df71c
jaqueline oliveira <jaqueline_s.oliveira27@hotmail.com>
2018-04-04 15:18:32 -0300

    Criacao da tela CadastrarCliente e CadastrarVendedor

--- VendaMotos/WpfView/CadastrarCliente.xaml.cs
+++ VendaMotos/WpfView/CadastrarCliente.xaml.cs
@@ -25,5 +25,10 @@ namespace WpfView
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
