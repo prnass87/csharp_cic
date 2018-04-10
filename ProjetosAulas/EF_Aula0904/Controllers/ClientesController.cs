d4fdad7729e6e558a8d4ec8ba5b171168a7a73b2
Paulo Roberto Nass <paulonass@hotmail.com>
2018-04-09 22:02:28 -0300

    Alterando o App.config

---
+++ ProjetosAulas/EF_Aula0904/Controllers/ClientesController.cs
@@ -0,0 +1,29 @@
+﻿using EF_Aula0904.Models;
+using EF_Aula0904.Models.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Aula0904.Controllers
+{
+    public class ClientesController
+    {
+        //INSERT
+        public static void SalvarCliente(Cliente cli)
+        {
+            MeuContexto bancoDados = new MeuContexto();
+
+            bancoDados.tblClientes.Add(cli);
+            bancoDados.SaveChanges();
+        }
+
+        //SELECT *
+        public static List<Cliente> ListarTodosClientes()
+        {
+            MeuContexto bancoDados = new MeuContexto();
+            return bancoDados.tblClientes.ToList();
+        }
+    }
+}
