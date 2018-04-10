35b7e182edc53f8904d047b02bd4041e9d99490a
Paulo Roberto Nass <paulonass@hotmail.com>
2018-04-09 21:13:47 -0300

    Instalação do entity, criação da DAL e classe de contexto

---
+++ ProjetosAulas/EF_Aula0904/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Aula0904.Models
+{
+    public class Cliente
+    {
+        public int ClienteID { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+    }
+}
---
+++ ProjetosAulas/EF_Aula0904/Models/DAL/MeuContexto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Aula0904.Models.DAL
+{
+    public class MeuContexto : DbContext
+    {
+
+        public MeuContexto() : base("strConn") //Nome da string de conexao
+        {
+
+        }
+
+        public DbSet<Cliente> tblClientes { get; set; }
+
+        
+
+    }
+}
