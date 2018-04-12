76c085f6d56974965827f8de2b13bb1fe83b131f
Paulo Roberto Nass <paulonass@hotmail.com>
2018-04-11 21:59:45 -0300

    Editando os botões da classe MeusVendedores

--- VendaMotos/WpfView/MeusClientes.xaml.cs
+++ VendaMotos/WpfView/MeusClientes.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Models;
+using Models.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +21,21 @@ namespace WpfView
     /// </summary>
     public partial class MeusClientes : Window
     {
+        Contexto ctx = new Contexto();
+        Cliente selecionado = null;
+
         public MeusClientes()
         {
             InitializeComponent();
+            try
+            {
+                dtgMeusClientes.ItemsSource = ctx.tblClientes.ToList();
+            }
+            catch (Exception)
+            {
+                
+            }
+            lblQuantidadeClientes.Content = ctx.tblClientes.Count();
         }
 
         private void btnNovoCliente_Click(object sender, RoutedEventArgs e)
@@ -30,15 +44,49 @@ namespace WpfView
             nova_tela.Show();
         }
 
-        private void btnExcluir_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
 
         private void dtgMeusClientes_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
+            try
+            {
+                selecionado = (Cliente)dtgMeusClientes.SelectedItem;
+                txtExcluir.Text = selecionado.Nome;
+                txtID.Text = selecionado.PessoaID.ToString();
+            }
+            catch (Exception)
+            {
 
+            }
         }
+
+        private void btnExcluir_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int ID = selecionado.PessoaID;
+                selecionado = (from c in ctx.tblClientes
+                               where c.PessoaID == ID
+                               select c).First();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Selecione um item para excluir!!");
+            }
+            if (selecionado == null)
+            {
+                return;
+            }
+            else
+            {
+                ctx.tblClientes.Remove(selecionado);
+                ctx.SaveChanges();
+                txtID.Text = "";
+                txtExcluir.Text = "";
+                dtgMeusClientes.ItemsSource = ctx.tblClientes.ToList();
+                lblQuantidadeClientes.Content = ctx.tblClientes.Count();
+            }
+        }
+
 
         private void btnEditar_Click(object sender, RoutedEventArgs e)
         {
